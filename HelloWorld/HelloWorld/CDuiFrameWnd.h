e6b0d71bf4eaf7e7ace1fd48ff4f501953e04e47
CMF <1176023110@qq.com>
2018-11-15 01:23:44 +0800

    cmf
    first commit

---
+++ HelloWorld/HelloWorld/CDuiFrameWnd.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <UIlib.h>
+using namespace DuiLib;
+
+class CDuiFrameWnd : public WindowImplBase
+{
+public:
+	virtual LPCTSTR    GetWindowClassName() const { return _T("DUIMainFrame"); }
+	virtual CDuiString GetSkinFile() { return _T("test1.xml"); }
+	virtual CDuiString GetSkinFolder() { return CPaintManagerUI::GetInstancePath() + _T("skin\\"); }
+
+};
+
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	CPaintManagerUI::SetInstance(hInstance);
+	CPaintManagerUI::SetResourcePath(CPaintManagerUI::GetInstancePath() + _T("skin\\"));
+	CDuiFrameWnd duiFrame;
+	duiFrame.Create(NULL, _T("DUIWnd"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+	duiFrame.CenterWindow();
+	duiFrame.ShowModal();
+	return 0;
+}
---
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,6 @@
+#include "stdafx.h"
+
+#include <windows.h>
+#include <tchar.h>
+#include "CDuiFrameWnd.h" //±¾´ÎÌí¼Ó
+
